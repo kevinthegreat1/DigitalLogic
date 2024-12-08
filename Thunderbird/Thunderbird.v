8587f9682fb1099576d98bf0e468dbaee13573c7
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2024-12-07 19:47:04 -0500

    Fix Thunderbird with full equations

--- Thunderbird/Thunderbird.v
+++ Thunderbird/Thunderbird.v
@@ -1,22 +1,40 @@
 module Thunderbird(input L, input R, input Reset, input CLK, output [2:0]Lout, output [2:0]Rout);
-	wire [2:0]LInternal, LbarInternal, Lnext, RInternal, RbarInternal, Rnext;
+	wire L0, L1, L2, Lbar0, Lbar1, Lbar2, R0, R1, R2, Rbar0, Rbar1, Rbar2;
+	wire [2:0]Lnext, Rnext;
+	wire LRterm1, LRterm2;
 
-	assign Lnext[0] = L&~R | ~R&LbarInternal[2]&LbarInternal[1]&LInternal[0] | ~R&LbarInternal[2]&LInternal[1]&LInternal[0] | (L~^R)&LbarInternal[2]&LInternal[1]&LbarInternal[0]&RInternal[0]&RbarInternal[1]&RInternal[2];
-	assign Lnext[1] = ~R&LbarInternal[2]&LbarInternal[1]&LInternal[0] | ~R&LbarInternal[2]&LInternal[1]&LInternal[0] | L&R;
-	assign Lnext[2] = ~R&~LInternal[2]&LInternal[1]&LInternal[0] | (L~^R)&LbarInternal[2]&LInternal[1]&LbarInternal[0]&RInternal[0]&RbarInternal[1]&RInternal[2];
+	assign LRterm1 = L&R & (~L2&~L1&~L0&~R0&~R1&~R2 | ~L2&~L1&L0&~R0&~R1&~R2 | ~L2&L1&L0&~R0&~R1&~R2 | L2&L1&L0&~R0&~R1&~R2
+								| ~L2&~L1&~L0&R0&~R1&~R2 | ~L2&~L1&~L0&R0&R1&~R2 | ~L2&~L1&~L0&R0&R1&R2 | L2&~L1&L0&~R0&R1&~R2);
+	assign LRterm2 = (L~^R) & ~L2&L1&~L0&R0&~R1&R2;
 
-	assign Rnext[0] = ~L&R | ~L&RInternal[0]&RbarInternal[1]&RbarInternal[2] | ~L&RInternal[0]&RInternal[1]&RbarInternal[2] | L&R;
-	assign Rnext[1] = ~L&RInternal[0]&RbarInternal[1]&RbarInternal[2] | ~L&RInternal[0]&RInternal[1]&RbarInternal[2] | (L~^R)&LbarInternal[2]&LInternal[1]&LbarInternal[0]&RInternal[0]&RbarInternal[1]&RInternal[2];
-	assign Rnext[2] = ~L&RInternal[0]&RInternal[1]&RbarInternal[2] | L&R;
+	assign Lnext[0] = ~R & (~L2&~L1&L0&~R0&~R1&~R2 | ~L2&L1&L0&~R0&~R1&~R2)
+						 | L&~R & (~L2&~L1&~L0&~R0&~R1&~R2 | ~L2&~L1&~L0&R0&~R1&~R2 | ~L2&~L1&~L0&R0&R1&~R2 | ~L2&~L1&~L0&R0&R1&R2
+									 | ~L2&L1&~L0&R0&~R1&R2 | L2&~L1&L0&~R0&R1&~R2)
+						 | LRterm2;
+	assign Lnext[1] = ~R & (~L2&~L1&L0&~R0&~R1&~R2 | ~L2&L1&L0&~R0&~R1&~R2)
+						 | LRterm1;
+	assign Lnext[2] = ~R & ~L2&L1&L0&~R0&~R1&~R2
+						 | LRterm2;
+	assign Rnext[0] = ~L & (~L2&~L1&~L0&R0&~R1&~R2 | ~L2&~L1&~L0&R0&R1&~R2)
+						 | ~L&R & (~L2&~L1&~L0&~R0&~R1&~R2 | ~L2&~L1&L0&~R0&~R1&~R2 | ~L2&L1&L0&~R0&~R1&~R2 | L2&L1&L0&~R0&~R1&~R2
+									 | ~L2&L1&~L0&R0&~R1&R2 | L2&~L1&L0&~R0&R1&~R2)
+						 | LRterm1;
+	assign Rnext[1] = ~L & (~L2&~L1&~L0&R0&~R1&~R2 | ~L2&~L1&~L0&R0&R1&~R2)
+						 | LRterm2;
+	assign Rnext[2] = ~L & ~L2&~L1&~L0&R0&R1&~R2
+						 | LRterm1;
 
-	DFlipFlop(~Reset & Lnext[0], CLK, LInternal[0], LbarInternal[0]);
-	DFlipFlop(~Reset & Lnext[1], CLK, LInternal[1], LbarInternal[1]);
-	DFlipFlop(~Reset & Lnext[2], CLK, LInternal[2], LbarInternal[2]);
+	DFlipFlop(~Reset & Lnext[0], CLK, L0, Lbar0);
+	DFlipFlop(~Reset & Lnext[1], CLK, L1, Lbar1);
+	DFlipFlop(~Reset & Lnext[2], CLK, L2, Lbar2);
+	DFlipFlop(~Reset & Rnext[0], CLK, R0, Rbar0);
+	DFlipFlop(~Reset & Rnext[1], CLK, R1, Rbar1);
+	DFlipFlop(~Reset & Rnext[2], CLK, R2, Rbar2);
 
-	DFlipFlop(~Reset & Rnext[0], CLK, RInternal[0], RbarInternal[0]);
-	DFlipFlop(~Reset & Rnext[1], CLK, RInternal[1], RbarInternal[1]);
-	DFlipFlop(~Reset & Rnext[2], CLK, RInternal[2], RbarInternal[2]);
-
-	assign Lout = LInternal;
-	assign Rout = RInternal;
+	assign Lout[0] = L0;
+	assign Lout[1] = L1;
+	assign Lout[2] = L2;
+	assign Rout[2] = R0;
+	assign Rout[1] = R1;
+	assign Rout[0] = R2;
 endmodule
--- Thunderbird/simulation/qsim/Thunderbird.do
+++ Thunderbird/simulation/qsim/Thunderbird.do
@@ -1,10 +1,10 @@
 onerror {exit -code 1}
 vlib work
 vlog -work work Thunderbird.vo
-vlog -work work Thunderbird.vwf.vt
-vsim -novopt -c -t 1ps -L cycloneive_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate work.Thunderbird_vlg_vec_tst -voptargs="+acc"
+vlog -work work ThunderbirdTest.vwf.vt
+vsim -novopt -c -t 1ps -L cycloneive_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate work.ThunderbirdTest_vlg_vec_tst -voptargs="+acc"
 vcd file -direction Thunderbird.msim.vcd
-vcd add -internal Thunderbird_vlg_vec_tst/*
-vcd add -internal Thunderbird_vlg_vec_tst/i1/*
+vcd add -internal ThunderbirdTest_vlg_vec_tst/*
+vcd add -internal ThunderbirdTest_vlg_vec_tst/i1/*
 run -all
 quit -f
